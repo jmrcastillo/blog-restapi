af98241cfc7e2fcc11d4c311d6c4e168d756b47f
dm <jm.cass20@gmail.com>
2020-05-14 15:12:04 +0800

    Admin - Remove built in installed apps

--- software/admin.py
+++ software/admin.py
@@ -1,8 +1,11 @@
 
 
 from django.contrib import admin
+from django.contrib.auth.models import Group, User
 
 from .models import Blog
 
 
 admin.site.register(Blog)
+admin.site.unregister(Group)
+admin.site.unregister(User)
